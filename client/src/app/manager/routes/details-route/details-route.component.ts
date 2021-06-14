6021c799717f712590fddcc6fb30e323833acafa
kuro1907 <vu.nguyennam.it@gmail.com>
2021-06-15 00:06:10 +0700

    CRUD Route

--- client/src/app/manager/routes/details-route/details-route.component.ts
+++ client/src/app/manager/routes/details-route/details-route.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Route } from 'src/app/_share/models/route.model';
+import { RouteService } from '../route.service';
 
 @Component({
   selector: 'app-details-route',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsRouteComponent implements OnInit {
 
-  constructor() { }
+  routes:Route;
+  detailFormRoute:FormGroup
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private routeService:RouteService,
+    private router:ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.router.snapshot.paramMap.get('id');
+    this.routeService.getRoute(id).subscribe(res=>this.routes = res);
+    this.initForm();
+    this.routeService.getRoute(id).subscribe(res=>this.pathRouteFormValue(res));
   }
 
+  initForm(){
+    this.detailFormRoute = this.formBuilder.group({
+      name:['',[Validators.required]],
+      number:['',[Validators.required]],
+      time_interval:['',Validators.required]
+    })
+  }
+
+  pathRouteFormValue(route:Route){
+    this.detailFormRoute.patchValue({
+      name : route.name,
+      number  : route.number,
+      time_interval : route.time_interval
+    })
+  }
 }
--- client/src/app/manager/routes/edit-route/edit-route.component.ts
+++ client/src/app/manager/routes/edit-route/edit-route.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Route } from 'src/app/_share/models/route.model';
+import { RouteService } from '../route.service';
 
 @Component({
   selector: 'app-edit-route',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditRouteComponent implements OnInit {
 
-  constructor() { }
+  editRouteForm:FormGroup;
+  routes:Route;
+  message:string;
+
+  constructor(
+    private routeService:RouteService,
+    private router:ActivatedRoute,
+    private formBuilder:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    const id = this.router.snapshot.paramMap.get('id');
+    this.routeService.getRoute(id).subscribe(res=>this.routes = res);
+    this.initForm();
+    this.routeService.getRoute(id).subscribe(res=>this.pathRouteFormValue(res));
+  }
+
+  initForm(){
+    this.editRouteForm = this.formBuilder.group({
+      name:['',[Validators.required]],
+      number:['',[Validators.required]],
+      time_interval:['',Validators.required]
+    })
+  }
+
+  pathRouteFormValue(route:Route){
+    this.editRouteForm.patchValue({
+      name : route.name,
+      number  : route.number,
+      time_interval : route.time_interval
+    })
+  }
+
+  updateRoute(){
+    if(this.editRouteForm.invalid){
+      return this.message = "Vui lòng kiểm tra lại thông tin";
+    }
+
+
+    const {value} = this.editRouteForm;
+
+    const id = this.router.snapshot.paramMap.get('id');
+
+    this.routeService.updateRoute(id,value).subscribe(
+      res => {
+        this.message = "Sửa thông tin xe thành công";
+        console.log(this.message);
+      },
+      err=>{
+        this.message = "Sửa thông tin xe không thành công, vui lòng thử lại!";
+        console.log(this.message);
+      }
+    )
   }
 
 }
