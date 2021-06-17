80250878e236c4c0d832d4208c169e8f35240536
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2021-06-17 13:59:02 +0700

    delay TripCancel

---
+++ client/src/app/operator/trip-cancel/trip-cancel.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TripCancelComponent } from './trip-cancel.component';
+
+describe('TripCancelComponent', () => {
+  let component: TripCancelComponent;
+  let fixture: ComponentFixture<TripCancelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TripCancelComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TripCancelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/operator/trip-cancel/trip-cancel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-cancel',
+  templateUrl: './trip-cancel.component.html',
+  styleUrls: ['./trip-cancel.component.scss']
+})
+export class TripCancelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
