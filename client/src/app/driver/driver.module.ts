904e028c3d95a03a009377819b25bf308f34626e
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2021-06-18 14:08:17 +0700

    create Driver Module and service / Update routingapp

--- client/src/app/driver/driver.module.ts
+++ client/src/app/driver/driver.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { DetailRouteComponent } from './detail-route/detail-route.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-
+const routes:Routes = [
+  {
+    path: '',
+    loadChildren:()=>import('../_share/profile/profile.module').then(m=>m.ProfileModule)
+  },
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  },
+  {
+    path:'detail-route/:id',
+    component: DetailRouteComponent
+  }
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [ScheduleComponent, DetailRouteComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule
   ]
 })
 export class DriverModule { }
